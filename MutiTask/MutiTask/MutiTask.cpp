e2df8e3407952a169632845a6a1947b9b6436c4d
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-01-14 00:02:49 +0900

    Update MutiTask.cpp

--- MutiTask/MutiTask/MutiTask.cpp
+++ MutiTask/MutiTask/MutiTask.cpp
@@ -23,7 +23,7 @@ public:
 	bool DoSomething() {
 		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-		for (int index = 0; index < 100000000; index++);
+		for (int index = 0; index < 20000000; index++);
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 		//std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
@@ -37,6 +37,14 @@ public:
 int main()
 {
 
+	unsigned int n = std::thread::hardware_concurrency();
+	std::cout << n << " concurrent threads are supported.\n";
+
+	unsigned int optimized_max_thread_count = (n - 3)*2;
+	unsigned int optimized_basic_thread_count = (n - 3) -5;
+
+	std::cout << n << "optimized_max_thread_count = " << optimized_max_thread_count << std::endl;
+	std::cout << n << "optimized_basic_thread_count = " << optimized_basic_thread_count << std::endl;
 
 	concurrency::concurrent_queue<std::shared_future<bool>> task_queue;
 
@@ -56,21 +64,38 @@ int main()
 	maker.detach();
 
 
-	std::thread consumer([&] {
-		while (true) {
-			std::cout << "task count = " << task_queue.unsafe_size() << std::endl;
-			std::shared_future<bool> task;
 
-			if (task_queue.try_pop(task)) {
-				auto result = task.get();
+	std::thread manager([&] {
+		for (int index = 0; index < optimized_basic_thread_count; index++) {
+			std::thread consumer([&] {
+				while (true) {
+					std::shared_future<bool> task;
+					if (task_queue.try_pop(task)) {
+						auto result = task.get();
+					}
+					std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				}
+			});
+			consumer.detach();
+		}
+
+
+		while (true) {
+			int task_count = task_queue.unsafe_size();
+			std::cout << "task count = " << task_count << std::endl;
+			if (task_count > 8) {
+				std::cout << "need optimization !!" << std::endl;
 			}
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	});
-	consumer.detach();
+	manager.detach();
+
+
+
 
 	while (true) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 
 }
